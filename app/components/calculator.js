46dbe7eb70ff86556314b655d2a61ee59bd95060
Eric LaPlante <ejlaplante@gmail.com>
2019-02-19 22:27:17 -0500

    basic calculator solves equations

--- app/components/calculator.js
+++ app/components/calculator.js
@@ -1,5 +1,22 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+	actions: {
+		clear() {
+			this.set('equation','');
+		},
+		input(value) {
+			var currentValue = this.equation;
+			this.set('equation', currentValue + value);
+		},
+		solve(string) {
+			string = string.replace('÷','/');
+			string = string.replace('×','*');
+			//string = string.replace('-','-');
+			//string = string.replace('+','+');
+			var solution = eval(string);
+			this.set('equation', solution);
+		}
 
+	}
 });
