6d9490ae22ea5eed89d4ce1d419d57bc384e50d5
Eric LaPlante <ejlaplante@gmail.com>
2019-03-03 15:35:22 -0500

    - Create service to set "activeTheme" at application level. - Fix "theme" integration test "document.getElementByID" returning null

--- app/components/theme.js
+++ app/components/theme.js
@@ -1,23 +1,37 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
 
 export default Component.extend({
+	activeThemeService: service('theme'),
 	classNames:['theme-component'],
 	setTheme(theme) {
+		//set active theme at application level
+		this.activeThemeService.set('activeTheme', theme);
+
+		//set active theme at component level
 		this.set('activeTheme', theme);
-		document.getElementById("cover-page").className = "bg-"+theme;
+
+		//set active theme in localstorage
 		localStorage.setItem('localActiveTheme', JSON.stringify(theme));
 	},
 	didInsertElement() {
 			let localStorageTheme = JSON.parse(localStorage.getItem('localActiveTheme'));
-
-			if (localStorageTheme) {
+			//if localstorage theme is set, use it
+			if (localStorageTheme) 
+			{
 				this.setTheme(localStorageTheme);
-			} else {
-				this.setTheme('light');
+			} 
+			//else get default theme from "services/theme"
+			else 
+			{
+				let defaultTheme = this.activeThemeService.get('activeTheme');
+				this.setTheme(defaultTheme);
 			}
 	},
 	actions: {
 		setThemeAction(theme) {
+			//if on homepage and select theme, send me to calculator page
 			if(window.location.pathname != '/calculator') {
 				document.getElementById('goToCalculator').click();
 			}
