44709e48d5024a2a8d9465d035c3da3a6768eba8
Eric LaPlante <ejlaplante@gmail.com>
2019-03-07 23:03:27 -0500

    integration test - calculator - click numbers, decimal and clear, set cell height

--- tests/integration/components/calculator-test.js
+++ tests/integration/components/calculator-test.js
@@ -6,7 +6,7 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | calculator', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('Calculator component renders - defaults to 0 - sets cellHeight - updates equation when numbers, decimal and clear buttons are clicked', async function(assert) {
 
     let defaultEquation = 0;
 
@@ -15,43 +15,29 @@ module('Integration | Component | calculator', function(hooks) {
 
     await render( hbs `<Calculator @equation={{equation}} @cellHeight={{cellHeight}}/>`);
 
-    //ON LOAD
+    //ON LOAD - DEFAULT TO ZERO, SET CELLHEIGHT
     assert.equal(this.equation, defaultEquation, 'onLoad - default equation is set to zero');
+    assert.notEqual(this.element.querySelector('td').getAttribute('height'), '', 'onLoad - Cell Height is set');
 
-    //ON CLICK - INDIVIDUAL NUMBERS AND CLEAR
+    //ON CLICK - INDIVIDUAL NUMBERS AND CLEAR BUTTONS
     let i;
     for(i = 0; i < 10; i++) {
       clickNumberButton(i, this);
     }
     function clickNumberButton(NumberButton, self){
       //click a number
-      let buttonNumber = NumberButton;
-      self.element.querySelector('.btn-' + buttonNumber ).click();
-      assert.equal(self.equation, buttonNumber, 'onClick - number ('+buttonNumber+') button   updates the equation to ( '+self.equation+' )');
+      self.element.querySelector('.btn-' + NumberButton ).click();
+      assert.equal(self.equation, NumberButton, 'onClick - "number" ('+NumberButton+') button updates the equation to ( '+self.equation+' )');
 
       //click clear
       self.element.querySelector('.btn-clear').click();
-      assert.equal(self.equation, defaultEquation, 'onClick - clear button resets the equation back to default value ( '+defaultEquation+')');
+      assert.equal(self.equation, defaultEquation, 'onClick - "clear" button resets the equation back to default value ( '+defaultEquation+')');
     }
 
+    //ON CLICK - DECIMAL BUTTON
+    this.element.querySelector('.btn-decimal').click();
+    assert.equal(this.equation, '0.', 'onClick - "decimal" button updates the equation to "0." ');
+
   });
-
-  /*test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{calculator}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#calculator}}
-        template block text
-      {{/calculator}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  }); */
 
 });
