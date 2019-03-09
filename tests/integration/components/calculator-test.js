2e909c6495cbd36e97abadf66ba2458d8e058424
Eric LaPlante <ejlaplante@gmail.com>
2019-03-08 23:17:18 -0500

    integration test - calculator - test all individual buttons, solve equations

--- tests/integration/components/calculator-test.js
+++ tests/integration/components/calculator-test.js
@@ -6,7 +6,8 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | calculator', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('Calculator component renders - defaults to 0 - sets cellHeight - updates equation when numbers, decimal, clear and operator buttons are clicked', async function(assert) {
+
+  test('It renders, defaults to 0, sets cellHeight', async function(assert) {
 
     let defaultEquation = 0;
 
@@ -18,8 +19,17 @@ module('Integration | Component | calculator', function(hooks) {
     //ON LOAD - DEFAULT TO ZERO, SET CELLHEIGHT
     assert.equal(this.equation, defaultEquation, 'onLoad - default equation is set to zero');
     assert.notEqual(this.element.querySelector('td').getAttribute('height'), '', 'onLoad - Cell Height is set');
+  });
 
-    //ON CLICK - INDIVIDUAL NUMBERS AND CLEAR BUTTONS
+
+  test('It updates equation when numbers, decimal, clear and operator buttons are clicked - also prevents consecutive operators and multiple decimals', async function(assert) {
+
+    let defaultEquation = 0;
+    this.set('equation', defaultEquation);
+
+    await render( hbs `<Calculator @equation={{equation}}/>`);
+
+    //ON CLICK - INDIVIDUAL NUMBERS
     let i;
     for(i = 0; i < 10; i++) {
       clickNumberButton(i, this);
@@ -29,7 +39,7 @@ module('Integration | Component | calculator', function(hooks) {
       self.element.querySelector('.btn-' + NumberButton ).click();
       assert.equal(self.equation, NumberButton, 'onClick - "number" ('+NumberButton+') button updates the equation to ( '+self.equation+' )');
 
-      //click clear
+      //clear
       self.element.querySelector('.btn-clear').click();
       assert.equal(self.equation, defaultEquation, 'onClick - "clear" button resets the equation back to default value ( '+defaultEquation+')');
     }
@@ -37,28 +47,52 @@ module('Integration | Component | calculator', function(hooks) {
     //ON CLICK - DECIMAL BUTTON
     await click('.btn-decimal');
     assert.equal(this.equation, '0.', 'onClick - "decimal" button updates the equation to "0." ');
+    await click('.btn-decimal');
+    assert.equal(this.equation, '0.', 'onClick - "decimal" prevents multiple decimals (ex 0..) ');
     this.element.querySelector('.btn-clear').click();
 
     //ON CLICK - OPERATOR BUTTONS
     //divide
     await click('.btn-divide');
     assert.equal(this.equation, '0÷', 'onClick - "divide" button updates the equation to "0÷" ');
-    this.element.querySelector('.btn-clear').click();
 
     //multiply
     await click('.btn-multiply');
-    assert.equal(this.equation, '0×', 'onClick - "multiply" button updates the equation to "0×" ');
-    this.element.querySelector('.btn-clear').click();
+    assert.equal(this.equation, '0×', 'onClick - "multiply" button updates the equation to "0×". no consecutive operators (ex 0÷×) ');
 
     //add
     await click('.btn-add');
-    assert.equal(this.equation, '0+', 'onClick - "add" button updates the equation to "0+" ');
-    this.element.querySelector('.btn-clear').click();
+    assert.equal(this.equation, '0+', 'onClick - "add" button updates the equation to "0+". no consecutive operators (ex 0×+)');
 
     //subtract
     await click('.btn-subtract');
-    assert.equal(this.equation, '0-', 'onClick - "subtract" button updates the equation to "0-" ');
-    this.element.querySelector('.btn-clear').click();
+    assert.equal(this.equation, '0-', 'onClick - "subtract" button updates the equation to "0-". no consecutive operators (ex 0+-) ');
+
+  });
+
+  test('It solves complex equations', async function(assert) {
+
+    this.set('equation', 0);
+    await render( hbs `<Calculator @equation={{equation}}/>`);
+
+    //operators: × ÷ - +  //the multiply symbol "×" is not the letter "x"
+
+    //basic equation
+    this.set('equation', '2×10+5');
+    await click('.btn-equals');
+    assert.equal(this.equation,'25', 'solved - 2x10+5=25');
+
+    //build new equation using previous answer
+    await click('.btn-subtract');
+    await click('.btn-5');
+    assert.equal(this.equation,'25-5', 'built new equation using previous answer');
+    await click('.btn-equals');
+    assert.equal(this.equation,'20', 'solved - equation using previous answer');
+    
+    //equation with decimals
+    this.set('equation', '1.1+2.22-3.333×4.4444÷5.55555');
+    await click('.btn-equals');
+    assert.equal(this.equation,'0.653623997624', 'solved - complex equation with decimals (1.1+2.22-3.333×4.4444÷5.55555=0.653623997624)');
 
   });
 
