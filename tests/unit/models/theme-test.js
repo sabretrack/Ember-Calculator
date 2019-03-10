81a0dd66bef45e7e87d59eba683a16576272e092
Eric LaPlante <ejlaplante@gmail.com>
2019-03-10 17:56:21 -0400

    documentation - theme component screen shot, add run loop to theme unit test

--- tests/unit/models/theme-test.js
+++ tests/unit/models/theme-test.js
@@ -1,16 +1,17 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { run } from '@ember/runloop';
 
 module('Unit | Model | theme', function(hooks) {
   setupTest(hooks);
 
-  test('retrieves name and class name', function(assert) {
+  test('retrieves theme name and class name', function(assert) {
     let store = this.owner.lookup('service:store');
-    let model = store.createRecord('theme', {
+    let model = run(() => store.createRecord('theme', {
 		'class-name': 'light',
 		name: "Light"
-    });
-    
+    }));
+
     assert.ok(model.get('class-name'), 'theme classname exists');
     assert.ok(model.get('name'), 'theme name exists');
 
